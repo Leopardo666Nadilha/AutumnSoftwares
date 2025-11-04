d486848c8304a1a54289fab3622699fb80e02375
Leonardo Padilha <leopadilha677@gmail.com>
2025-11-03 22:53:13 -0300

    chore: Stop tracking .next directory

--- Front-end/.next/dev/server/pages/_app.js
+++
@@ -1,9 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/node_modules_next_2a0e4ecb._.js")
-R.c("server/chunks/ssr/node_modules_react-icons_bs_index_mjs_ad797dda._.js")
-R.c("server/chunks/ssr/node_modules_react-icons_fa_index_mjs_57cf400d._.js")
-R.c("server/chunks/ssr/node_modules_react-icons_lib_d9e6764d._.js")
-R.c("server/chunks/ssr/node_modules_@swc_helpers_cjs_8ee28cf2._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__476d6477._.js")
-R.m("[project]/pages/_app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/pages/_app.js [ssr] (ecmascript)").exports
